d70608d318560998dcf307942af03425ecfac193
Da-6y9eT-c8eT-LukushinAV <lukushinav@mer.ci.nsu.ru>
2025-02-10 17:43:59 +0700

    Вариант 1

---
+++ CarServiceApp/CarServiceApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ContextLibrary;
+using ContextLibrary.Entities;
+using System.Linq;
+using System.Windows;
+using CarServiceApp;
+
+namespace CarServiceApp
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ApplicationContext _context;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _context = new ApplicationContext(); // создаем экземпляр контекста
+            LoadRequests();
+        }
+
+        // Загружаем заявки в DataGrid
+        private void LoadRequests()
+        {
+            RequestsDataGrid.ItemsSource = _context.Requests; // используем экземпляр _context
+        }
+
+        // Открытие окна добавления заявки
+        private void AddRequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            var requestWindow = new RequestWindow();
+            requestWindow.ShowDialog(); // Показываем окно добавления заявки
+            LoadRequests(); // Перезагружаем список заявок
+        }
+
+        // Открытие окна редактирования заявки
+        private void EditRequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RequestsDataGrid.SelectedItem is Request selectedRequest)
+            {
+                var requestWindow = new RequestWindow(selectedRequest); // Передаем выбранную заявку
+                requestWindow.ShowDialog();
+                LoadRequests(); // Перезагружаем список заявок
+            }
+            else
+            {
+                MessageBox.Show("Выберите заявку для редактирования.");
+            }
+        }
+
+
+    }
+}
---
+++ CarServiceApp/CarServiceApp/RequestWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using ContextLibrary;
+using ContextLibrary.Entities;
+using ContextLibrary.Enums;
+using System;
+using System.Windows;
+
+namespace CarServiceApp
+{
+    public partial class RequestWindow : Window
+    {
+        private Request? _currentRequest;
+        private readonly ApplicationContext _context;
+
+        public RequestWindow(Request? request = null)
+        {
+            InitializeComponent();
+            _context = new ApplicationContext(); // создаем экземпляр контекста
+
+            _currentRequest = request;
+
+            if (_currentRequest != null)
+            {
+                // Заполняем поля для редактирования
+                NumberTextBox.Text = _currentRequest.Number.ToString();
+                AddedDateTextBox.Text = _currentRequest.AddedDate.ToString("dd.MM.yyyy");
+                CarTypeComboBox.SelectedItem = CarTypeComboBox.Items[_currentRequest.CarType.GetHashCode()];
+                CarModelTextBox.Text = _currentRequest.CarModel;
+                DescriptionTextBox.Text = _currentRequest.Description;
+                ClientLFPTextBox.Text = _currentRequest.ClientLFP;
+                PhoneNumberTextBox.Text = _currentRequest.PhoneNumber;
+                StatusComboBox.SelectedItem = StatusComboBox.Items[_currentRequest.Status.GetHashCode()];
+            }
+            else
+            {
+                // Для нового запроса генерируем номер
+                NumberTextBox.Text = _context.GenerateRequestNumber().ToString();
+                AddedDateTextBox.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentRequest == null)
+            {
+                // Создаём новую заявку
+                var newRequest = new Request
+                {
+                    Number = int.Parse(NumberTextBox.Text),
+                    AddedDate = DateTime.Parse(AddedDateTextBox.Text),
+                    CarType = (CarType)CarTypeComboBox.SelectedIndex,
+                    CarModel = CarModelTextBox.Text,
+                    Description = DescriptionTextBox.Text,
+                    ClientLFP = ClientLFPTextBox.Text,
+                    PhoneNumber = PhoneNumberTextBox.Text,
+                    Status = (RequestStatus)StatusComboBox.SelectedIndex
+                };
+
+                _context.Requests.Add(newRequest); // используем экземпляр _context
+            }
+            else
+            {
+                // Редактируем существующую заявку
+                _currentRequest.CarType = (CarType)CarTypeComboBox.SelectedIndex;
+                _currentRequest.CarModel = CarModelTextBox.Text;
+                _currentRequest.Description = DescriptionTextBox.Text;
+                _currentRequest.ClientLFP = ClientLFPTextBox.Text;
+                _currentRequest.PhoneNumber = PhoneNumberTextBox.Text;
+                _currentRequest.Status = (RequestStatus)StatusComboBox.SelectedIndex;
+            }
+
+            MessageBox.Show("Заявка сохранена!");
+            Close();
+        }
+    }
+
+}
